008fb1fe3679a2da9fc5469df76a266cc19ab91e
Babaloricha <51730856+Babaloricha@users.noreply.github.com>
2025-04-04 11:48:46 +0000

    ajustado a conexao funcionando ok

--- infra/database.js
+++ infra/database.js
@@ -1,14 +1,21 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
-  const client = new Client();
-  await client.connect({
-    host: "localhost",
+  const { Client } = require("pg");
+
+  const client = new Client({
+    host: "localhost", // ou o nome do container, tipo 'postgres'
     port: 5432,
     user: "postgres",
-    database: "postgres",
     password: "localpassword",
+    database: "postgres",
   });
+
+  client
+    .connect()
+    .then(() => console.log("Conectado ao PostgreSQL!"))
+    .catch((err) => console.error("Erro de conexão:", err.stack));
+
   const result = await client.query(queryObject);
   await client.end();
   return result;
