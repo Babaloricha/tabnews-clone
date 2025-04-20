75d5b13b369b0296db13592483bfda3094a28a03
Babaloricha <51730856+Babaloricha@users.noreply.github.com>
2025-04-20 17:08:26 +0000

    feat: adicionando mas  atributos ao status

--- pages/api/v1/status.js
+++ pages/api/v1/status.js
@@ -1,9 +1,28 @@
 import database from "infra/database.js";
+import { version } from "react";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1+1 as soma;");
-  console.log(result.rows);
-  response.status(200).send("ta funcionando legal");
+  const updatedAt = new Date().toISOString();
+
+  //const result = await database.query("SELECT 1+1;");
+  //console.log(result);
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionValue =
+    databaseMaxConnectionResult.rows[0].max_connections;
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionValue),
+      },
+    },
+  });
 }
 
 export default status;
